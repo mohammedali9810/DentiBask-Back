e1c35f97404ace71f7c4eae1422314a0f6bc1b73
mohammedali9810 <mohammedali9810@gmail.com>
2023-11-05 09:35:20 +0200

    Some Edits to the Users-Orders Models.

--- User/models.py
+++ User/models.py
@@ -6,38 +6,35 @@ from django.contrib.auth.models import User
 
 ############---------Customer MODEL---------############
 class Customer(models.Model):
-    id = models.AutoField(User, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=200)
-    # about = models.TextField()
     email = models.EmailField()
     phone = models.CharField(max_length=11,
         validators=[
             RegexValidator(
                 regex="^01[0|1|2|5][0-9]{8}$",
-                message="Phone must be start 010, 011, 012, 015 and all number contains 11 digits",
+                message="Phone must start with 010, 011, 012, or 015 and contain 11 digits",
                 code="invalid number",
             )
         ], blank=True)
 
     def __str__(self):
-        return f"{self.user.username}"
+        return self.name
 
 
 ############---------Vendor MODEL---------############
 class Vendor(models.Model):
-    id = models.AutoField(User, on_delete=models.CASCADE,primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=200)
-    #about = models.TextField()
     email = models.EmailField()
-    phone = models.IntegerField(max_length=11,
+    phone = models.CharField(max_length=11,
         validators=[
             RegexValidator(
                 regex="^01[0|1|2|5][0-9]{8}$",
-                message="Phone must be start 010, 011, 012, 015 and all number contains 11 digits",
+                message="Phone must start with 010, 011, 012, or 015 and contain 11 digits",
                 code="invalid number",
             )
         ], blank=True)
-
 
     def __str__(self):
         return self.name
@@ -45,26 +42,33 @@ class Vendor(models.Model):
 ############---------Payment-Info MODEL---------############
 
 class Pay_inf(models.Model):
-    User_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     card_name = models.CharField(max_length=200)
-    exp_dat = models.DateField()
+    exp_date = models.DateField()
 
 ############---------User/Vendor-Info MODEL---------############
 class Add_info(models.Model):
-    vendor = models.OneToOneField(Vendor, on_delete=models.CASCADE)
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
+    vendor = models.OneToOneField(Vendor, on_delete=models.CASCADE, null=True, blank=True)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, null=True, blank=True)
     birth = models.DateField()
     address = models.TextField()
-    pay_inf = models.ForeignKey(Pay_inf)
+    pay_inf = models.ForeignKey(Pay_inf, on_delete=models.CASCADE)
     country = models.CharField(max_length=40, blank=True)
-    image = models.ImageField(
-         default="default.jpg", upload_to="profile_pics", blank=True
-    )
+    image = models.ImageField(default="default.jpg", upload_to="profile_pics", blank=True)
+
     def __str__(self):
-        return self.name
+        if self.vendor:
+            return f"Vendor: {self.vendor.name}"
+        elif self.customer:
+            return f"Customer: {self.customer.name}"
+        else:
+            return "No associated user"
 
 ############---------ِOrder MODEL---------############
-class Order (models.Model):
+class Order(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     order_date = models.DateField()
+
+    def __str__(self):
+        return f"Order for {self.user.username}"

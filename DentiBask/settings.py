3f9aef82532ed309ad3fc3296556dcc452968c15
shaher2017 <shaherhero8@gmail.com>
2023-11-05 22:25:23 +0200

    Modification on initialization

--- DentiBask/settings.py
+++ DentiBask/settings.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-<<<<<<< HEAD
+
 import os.path
-=======
+
 import os
->>>>>>> Ashraf
+
 from pathlib import Path
 from django.contrib.messages import constants as messages
 
@@ -42,17 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
     'User',
     'Products',
     'rest_framework',
-    'django_cleanup'
-=======
-    'rest_framework',
-    'Products',
-    'User',
+    'django_cleanup',
     'api'
->>>>>>> Ashraf
 ]
 
 MIDDLEWARE = [
@@ -91,21 +85,12 @@ WSGI_APPLICATION = 'DentiBask.wsgi.application'
 
 DATABASES = {
     'default': {
-<<<<<<< HEAD
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dentibask',
-        'USER' : '',
-        'PASSWORD' : '',
+        'USER' : 'postgres',
+        'PASSWORD' : 'admin',
         'HOST' : 'localhost',
         'PORT' : '5432'
-=======
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
->>>>>>> Ashraf
     }
 }
 
--- User/models.py
+++ User/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from Products.models import Product
 from django.core.validators import RegexValidator
@@ -22,25 +24,7 @@ class Customer(models.Model):
         return self.name
 
 
-############---------Vendor MODEL---------############
-class Vendor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    name = models.CharField(max_length=200)
-    email = models.EmailField()
-    phone = models.CharField(max_length=11,
-        validators=[
-            RegexValidator(
-                regex="^01[0|1|2|5][0-9]{8}$",
-                message="Phone must start with 010, 011, 012, or 015 and contain 11 digits",
-                code="invalid number",
-            )
-        ], blank=True)
-
-    def __str__(self):
-        return self.name
-
 ############---------Payment-Info MODEL---------############
-
 class Pay_inf(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     card_name = models.CharField(max_length=200)
@@ -48,7 +32,7 @@ class Pay_inf(models.Model):
 
 ############---------User/Vendor-Info MODEL---------############
 class Add_info(models.Model):
-    vendor = models.OneToOneField(Vendor, on_delete=models.CASCADE, null=True, blank=True)
+    # vendor = models.OneToOneField(Vendor, on_delete=models.CASCADE, null=True, blank=True)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, null=True, blank=True)
     birth = models.DateField()
     address = models.TextField()
@@ -64,23 +48,69 @@ class Add_info(models.Model):
         else:
             return "No associated user"
 
-############---------ِOrder MODEL---------############
+################################################################################################################################
+
+############---------Order MODEL---------############
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    order_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Order for {self.user.username}"
-
-############---------Cart MODEL---------############
-class Cart(models.Model):
-    id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-############---------Cart-Items MODEL---------############
-class Cart_Items(models.Model):
-    id = models.AutoField(primary_key=True)
-    cart_id = models.OneToOneField(Cart, on_delete=models.CASCADE)
+############---------OrderItem MODEL---------############
+class OrderItem(models.Model):
+    order_id = models.ForeignKey(Order)
     product_id = models.ForeignKey(Product)
+    product_order_price = models.FloatField()
     quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        unique_together = ('order_id', 'product_id')
+
+############---------Clinic MODEL---------############
+class Clinic(models.Model):
+    id = models.AutoField(primary_key=True)
+    location = models.CharField(max_length=50)
+    area = models.FloatField()
+    price = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+############---------Rent MODEL---------############
+class Rent(models.Model):
+    id = models.AutoField(primary_key=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    duration_months = models.IntegerField()
+    price = models.FloatField()
+    renter = models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def save(self, *args, **kwargs):
+        # Calculate duration in months based on start_date and end_date
+        if self.start_date and self.end_date:
+            self.duration_months = (
+                    (self.end_date.year - self.start_date.year) * 12
+                    + (self.end_date.month - self.start_date.month)
+            )
+        super(Rent, self).save(*args, **kwargs)
+
+
+
+## Not Needed !!!
+# ############---------Vendor MODEL---------############
+# class Vendor(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+#     name = models.CharField(max_length=200)
+#     email = models.EmailField()
+#     phone = models.CharField(max_length=11,
+#         validators=[
+#             RegexValidator(
+#                 regex="^01[0|1|2|5][0-9]{8}$",
+#                 message="Phone must start with 010, 011, 012, or 015 and contain 11 digits",
+#                 code="invalid number",
+#             )
+#         ], blank=True)
+#     def __str__(self):
+#         return self.name
+
+## not needed !!!

f824e924c49ef7bf89a92ea92636bf6cc2543705
shaher2017 <shaherhero8@gmail.com>
2023-11-07 15:26:02 +0200

    adding status

--- User/models.py
+++ User/models.py
@@ -55,6 +55,13 @@ class Add_info(models.Model):
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    STATUS_CHOICES = [
+        ('Cancelled', 'Cancelled'),
+        ('Processing', 'Processing'),
+        ('Shipped', 'Shipped'),
+        ('Delivered', 'Delivered'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Processing')
     created_at = models.DateTimeField(auto_now_add=True)
 
 ############---------OrderItem MODEL---------############

a6084b5649a1a359afcb36a4ffe31a06d4ad18e0
mohammedali9810 <mohammedali9810@gmail.com>
2023-11-14 00:35:35 +0200

    Edits to User Urls.

--- User/urls.py
+++ User/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .api import (OrderViewSet, OrderItemViewSet, ClinicViewSet, CustomerViewSet, RentViewSet, AddInfoViewSet,
-<<<<<<< HEAD
-                  PayInfoViewSet, MyObtainToken, check_email, register, activate_account)
-=======
-                  PayInfoViewSet, MyObtainToken, check_email,add_clinic,get_user_clinic)
->>>>>>> 219cd2465c6160848d7ede8e9238bf659bb682a4
+                  PayInfoViewSet, MyObtainToken, check_email, register, activate_account,add_clinic,get_user_clinic)
 
 router = DefaultRouter()
 router.register('order', OrderViewSet)
@@ -20,12 +16,9 @@ urlpatterns = [
     path('', include(router.urls)),
     path('login/', MyObtainToken.as_view(), name='token_obtain_pair'),
     path('checkemail/', check_email, name='check_email'),
-<<<<<<< HEAD
     path('activate/<uidb64>/<token>/', activate_account, name='activate_account'),
     path('api/register/', register, name='register')
-=======
     path('addclinic/', add_clinic, name='add_clinic'),
     path('userclinic/', get_user_clinic, name='get_user_clinic'),
 
->>>>>>> 219cd2465c6160848d7ede8e9238bf659bb682a4
 ]

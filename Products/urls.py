5184e04b2ecbaed17c752f83c004022c56e4756b
shaher2017 <shaherhero8@gmail.com>
2023-11-14 23:07:02 +0200

    fix S3 image deleting and Edit Product

--- Products/urls.py
+++ Products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .api import ProductViewSet, CategoryViewSet, get_csrf_token,get_all_products
+from .api import ProductViewSet, CategoryViewSet, get_csrf_token,get_all_products,update_product
 
 router = DefaultRouter()
 router.register(r'products',ProductViewSet)
@@ -8,4 +8,5 @@ router.register(r'category',CategoryViewSet)
 
 urlpatterns=[path('', include(router.urls)),path('get_csrf_token/',get_csrf_token)
 ,path('get_products/',get_all_products)
+,path('update_product/',update_product)
              ]

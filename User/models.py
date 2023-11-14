d1f2f20567b81627264bb2f493387133244f29ad
mohammedali9810 <mohammedali9810@gmail.com>
2023-11-14 20:18:18 +0200

    Is active

--- User/models.py
+++ User/models.py
@@ -25,7 +25,7 @@ class Customer(models.Model):
             )
         ], blank=True)
     image = models.ImageField(upload_to=unique_image_customer)
-
+    is_active = models.BooleanField(default=False)
     def __str__(self):
         return self.email
 

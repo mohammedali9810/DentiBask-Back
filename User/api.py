64be283948639ca5be5f109d39d5680362450a84
mohammedali9810 <mohammedali9810@gmail.com>
2023-11-28 17:32:23 +0200

    Other Fixes to O Auth & Jwt.

--- User/api.py
+++ User/api.py
@@ -48,6 +48,7 @@ from google.oauth2 import id_token
 from google.auth.transport.requests import Request
 import jwt
 import requests
+from django.contrib.auth.models import User
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
@@ -220,6 +221,12 @@ def check_reg(request):
 
 # Your existing Customer model, CustomerSerializer, and account_activation_token imports go here
 #################################################################################################
+class MyOauthObtainToken(TokenObtainPairView):
+    @classmethod
+    def post(self, request, *args, **kwargs):
+        role = 'user'
+        token = super().post(request, *args, **kwargs).data
+        return Response({"token": token, "role": role, "is_authenticated" :True})
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -256,44 +263,46 @@ def google_signin(request):
         existing_customer = Customer.objects.filter(email=email).first()
         print(existing_customer)
         if existing_customer:
+            token_data = generate_jwt_token(user)
             # Email exists, attempt login
             print("USER EXISTO")
-            user = authenticate(username=email, password='')  # No password needed for social login
-            print(f"USER AUTHO : {user}")
-            if user is not None:
-                # Login successful
-                if user.username == "oem":
-                    role = 'admin'
-                else:
-                    role = 'user'
-
-                token = RefreshToken.for_user(user).access_token
-                return Response({"token": token, "role": role, "is_authenticated": True})
-            else:
-                # Invalid credentials
-                return Response({"error": "Invalid social login credentials.", "is_authenticated": False}, status=status.HTTP_401_UNAUTHORIZED)
+            # user = authenticate(username=email, password='')  # No password needed for social login
+            # print(f"USER AUTHO : {user}")
+            # if user is not None:
+            #     # Login successful
+            #     if user.username == "oem":
+            #         role = 'admin'
+            #     else:
+            #         role = 'user'
+            #
+            #     token = RefreshToken.for_user(user).access_token
+            #     return Response({"token": token, "role": role, "is_authenticated": True})
+            # else:
+            #     # Invalid credentials
+            #     return Response({"error": "Invalid social login credentials.", "is_authenticated": False}, status=status.HTTP_401_UNAUTHORIZED)
         else:
             # Email does not exist, proceed with registration
             serializer = Cust_signin_ser(data={'email': email, 'name': name})
             if serializer.is_valid():
                 customer = serializer.save()
+                MyOauthObtainToken.post(request)
                 # Generate token for the newly registered user
-                user = authenticate(username=email, password=None)
-                token = RefreshToken.for_user(user).access_token
+                # user = authenticate(username=email, password=None)
+                # token = RefreshToken.for_user(user).access_token
 
                 # You can include the activation email logic here if needed
 
                 # Provide a success response with a redirect URL and message
                 # redirect_url = reverse('activate', args=[urlsafe_base64_encode(force_bytes(customer.pk)),
                 #                                          account_activation_token.make_token(customer)])
-                success_message = 'Check your email to activate your account.'
-                return Response({
-                    # 'redirect_url': redirect_url,
-                    'token': token,
-                    'role': 'user',  # You may customize the role as needed
-                    'is_authenticated': True,
-                    'success_message': success_message,
-                }, status=status.HTTP_201_CREATED)
+                # success_message = 'Check your email to activate your account.'
+                # return Response({
+                #     # 'redirect_url': redirect_url,
+                #     'token': token,
+                #     'role': 'user',  # You may customize the role as needed
+                #     'is_authenticated': True,
+                #     'success_message': success_message,
+                # }, status=status.HTTP_201_CREATED)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
